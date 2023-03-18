323c69526fa824bd219a74db29984c191a7d6946
mendrika <randrianarisonmendrikanyaina@gmail.com>
2023-03-18 12:12:52 +0300

    redeploy

--- src/main/java/app/article/repository/ArticleRepos.java
+++
@@ -1,8 +0,0 @@
-package app.article.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.article.model.Article;
-
-public interface ArticleRepos extends JpaRepository<Article, Integer> {
-}
--- src/main/java/app/article/repository/CategoryRepos.java
+++
@@ -1,9 +0,0 @@
-package app.article.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.article.model.Category;
-
-public interface CategoryRepos extends JpaRepository<Category, Integer> {
-
-}
